63c63e74f2fd635ec7d71f44cf6ecae777a31783
JiwooKim-dev <jiwoo.kim.dev@gmail.com>
2020-04-02 00:04:42 +0900

    Add Lab 5D ViewPager

---
+++ Lab5D_ViewPager/app/src/main/java/com/example/lab5d_viewpager/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.lab5d_viewpager;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.lab5d_viewpager.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Fragment[] arrFragments = new Fragment[3];
+        arrFragments[0] = new RedFragment();
+        arrFragments[1] = new YellowFragment();
+        arrFragments[2] = new GreenFragment();
+
+        ViewPager viewpager = findViewById(R.id.vp_pager);
+        MyPagerAdapter adapter = new MyPagerAdapter(getSupportFragmentManager(), arrFragments);
+
+        viewpager.setAdapter(adapter);
+    }
+
+    private class MyPagerAdapter extends FragmentPagerAdapter {
+
+        private Fragment[] arrFragments;
+
+        public MyPagerAdapter(FragmentManager fm, Fragment[] arrFragments) {
+            super(fm);
+            this.arrFragments = arrFragments;
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            return arrFragments[position];
+        }
+
+        @Override
+        public int getCount() {
+            return arrFragments.length;
+        }
+    }
+}
