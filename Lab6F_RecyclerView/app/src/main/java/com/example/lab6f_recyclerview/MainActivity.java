67ccb513e1f57db1ec40e928254d69c1961723f9
JiwooKim-dev <jiwoo.kim.dev@gmail.com>
2020-04-02 19:14:59 +0900

    Add Lab 6F RecyclerView (Gradle)

---
+++ Lab6F_RecyclerView/app/src/main/java/com/example/lab6f_recyclerview/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.lab6f_recyclerview;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.example.lab6f_recyclerview.model.PostItem;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        /* 포스트 데이터 입력 */
+        ArrayList<PostItem> listItem = new ArrayList<>();
+
+        for (int i=0 ; i<30 ; i++){
+
+            PostItem postItem = new PostItem(true, i*10,"user"+(i+1),
+                    "https://images-na.ssl-images-amazon.com/images/I/51aq7G0ttnL._AC_SY606_.jpg",
+                    "This is item "+(i+1));
+            listItem.add(i, postItem);
+        }
+
+        /* 포스트 화면에 출력 */
+
+        RecyclerView rvList = findViewById(R.id.rv_list);
+
+    }
+}
