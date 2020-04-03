33170d732071e6d786deae352a7f6ff079f5f8cd
JiwooKim-dev <jiwoo.kim.dev@gmail.com>
2020-04-03 18:03:07 +0900

    Add Lab 7C AsyncTask

---
+++ Lab7C_AsyncTask/app/src/main/java/com/example/lab7c_asynctask/MainActivity.java
@@ -0,0 +1,74 @@
+package com.example.lab7c_asynctask;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private TextView tvDisplay;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvDisplay = findViewById(R.id.tv_display);
+        Button btnStart = findViewById(R.id.btn_start);
+        btnStart.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        startAsyncCalc();
+    }
+
+    private void startAsyncCalc() {
+
+        AsyncCalcTask task = new AsyncCalcTask();
+        task.execute(1, Integer.MAX_VALUE);
+    }
+
+    class AsyncCalcTask extends AsyncTask<Integer, Integer, Integer>{
+
+        /* Async Thread Method */
+        @Override
+        protected Integer doInBackground(Integer... integers) {
+
+            int number = integers[0];
+            int count = integers[1];
+            int result = 0;
+
+            int percentUnit = count / 100;
+
+            for (int i=0 ; i<count ; i++){
+                result += number;
+
+                if (result % percentUnit == 0){
+                    publishProgress(result/percentUnit);
+                }
+            }
+            return result;
+        }
+
+        /* UI Thread Method (실행중) */
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+            tvDisplay.setText(values[0] + "%");
+        }
+
+        /* UI Thread Method (실행결과) */
+        @Override
+        protected void onPostExecute(Integer integer) {
+            super.onPostExecute(integer);
+
+            tvDisplay.setText("Result : " + integer);
+        }
+    }
+}
